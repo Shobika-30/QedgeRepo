3c06e1f69ac327d4f371fdaa25bb9a550e52e80b
Shobika_30 <shobika.s73@gmail.com>
2024-10-05 08:13:30 +0530

    feature202 changes have been added

--- Application.java
+++ Application.java
@@ -8,6 +8,12 @@ public class SwapNumbers {
         System.out.println("First number = " + first);
         System.out.println("Second number = " + second);
 
+        #feature202 changes by ethan
+        String txt = "Hello World";
+        System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+        System.out.println(txt.toLowerCase());   // Outputs "hello world"
+        #end of feature202
+        
         // Value of first is assigned to temporary
         float temporary = first;
 
